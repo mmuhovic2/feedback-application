357057fc8ebaa7b1eb475834327b167b8fe01649
mdokic1 <mdokic1@etf.unsa.ba>
2021-05-22 20:35:09 +0200

    Multiple choice question

--- questionScreens/MultipleChoiceQuestionScreen.js
+++ questionScreens/MultipleChoiceQuestionScreen.js
@@ -1,16 +1,65 @@
 import React, { useState } from 'react'
-import { Text, View, StyleSheet } from 'react-native';
+import { Text, View, StyleSheet, Dimensions } from 'react-native';
 import ButtonContainer from '../ButtonContainer';
+import * as Animatable from 'react-native-animatable';
+import { RadioButton } from 'react-native-paper';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
 
 const MultipleChoiceQuestionScreen = ({ question }) => {
-  const [answer, setAnswer] = useState("...");
+  const [answer, setAnswer] = useState(question.QuestionAnswers[0].AnswerId);
+  const [checked, setChecked] = React.useState('first');
 
   return (
     <View>
       <View style={styles.question}>
-        <Text>This is a multiple choice question</Text>
-        <Text>{question.QuestionText}</Text>
-      </View>
+        <Text style={styles.text_question}>{question.QuestionText}</Text>
+        <Text></Text>
+        <View style={styles.action}>
+          
+           <View style={styles.button_container}>
+               <View style={{flex:0}}>
+                  <RadioButton
+                    // style={{marginRight: 50}}
+                    value="first"
+                    status={ checked === 'first' ? 'checked' : 'unchecked' }
+                    onPress={() => {setChecked('first'); setAnswer(question.QuestionAnswers[0].AnswerId)}}
+                  />
+                </View> 
+                <View style={{flex:1}}>
+                  <Text style={styles.text_footer}>{question.QuestionAnswers[0].Answer.AnswerText}</Text> 
+                </View> 
+           </View> 
+
+           <View style={styles.button_container}>
+               <View style={{flex:0}}>
+                  <RadioButton
+                    // style={{marginRight: 50}}
+                    value="second"
+                    status={ checked === 'second' ? 'checked' : 'unchecked' }
+                    onPress={() => {setChecked('second'); setAnswer(question.QuestionAnswers[1].AnswerId)}}
+                  />
+                </View> 
+                <View style={{flex:1}}>
+                  <Text style={styles.text_footer}>{question.QuestionAnswers[1].Answer.AnswerText}</Text> 
+                </View> 
+           </View> 
+
+           <View style={styles.button_container}>
+               <View style={{flex:0}}>
+                  <RadioButton
+                    // style={{marginRight: 50}}
+                    value="third"
+                    status={ checked === 'third' ? 'checked' : 'unchecked' }
+                    onPress={() => {setChecked('third'); setAnswer(question.QuestionAnswers[2].AnswerId)}}
+                  />
+                </View> 
+                <View style={{flex:1}}>
+                  <Text style={styles.text_footer}>{question.QuestionAnswers[2].Answer.AnswerText}</Text> 
+                </View> 
+           </View> 
+                        
+        </View>
+     </View>
       <ButtonContainer answer={answer} />
     </View>
   )
@@ -23,4 +72,22 @@ const styles = StyleSheet.create({
     height: '90%',
     justifyContent: 'center'
   },
+  text_footer: {
+    color: '#05375a',
+    fontSize: 18
+  },
+  action: {
+    flexDirection: 'column',
+    marginTop: 10,
+    borderBottomWidth: 1,
+    borderBottomColor: '#f2f2f2',
+    paddingBottom: 5
+  },
+  button_container: {
+    flexDirection: "row"
+  },
+  text_question: {
+    color: "#fff",
+    fontSize: 25
+  }
 });
